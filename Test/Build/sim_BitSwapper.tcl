7ea75550e492a9fcd03d0a777668ceefad6e5f1f
wisemankaitlyn <68436733+wisemankaitlyn@users.noreply.github.com>
2021-09-24 14:37:32 -0400

    (#2) fixes to sim_BitSwapper.tcl
    - fixes to sim_BitSwapper.tcl based on testing done during debugging for issue #2

--- Test/Build/sim_BitSwapper.tcl
+++ Test/Build/sim_BitSwapper.tcl
@@ -1,11 +1,13 @@
 vlib work
 vmap work
 
-vcom ../../Source/BitSwapper.vhd
-vcom ../Source/BitSwapper_tb.vhd
+vcom -work work ../../Source/BitSwapper.vhd
+vcom -work work ../Source/BitSwapper_tb.vhd
 
-#vsim BitSwapper_tb
+vsim work.BitSwapper_tb
 
-#add wave -group {UUT} BitSwapper_tb/Uut/*
+add wave -group {TB}  BitSwapper_tb/*
+add wave -group {UUT} BitSwapper_tb/Uut/*
 
-#run -all
+run -all
+echo "done!"
